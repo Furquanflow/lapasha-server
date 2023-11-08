09fdebfdae4f93e84cc85e97db3732872e27b5f3
Furquanflow <furqan.rahim@flowtechnologies.io>
2023-11-08 06:10:50 +0500

    Develope Api

--- router/Router.js
+++ router/Router.js
@@ -1,8 +1,21 @@
 const express = require("express");
-const { getFormData, saveFormData } = require("../controller/Controller");
+const {
+  getFormData,
+  saveFormData,
+  getLoungeAndGrillData,
+  saveLoungeAndGrillData,
+  getNaraCafeData,
+  saveNaraCafeData
+} = require("../controller/Controller");
 const router = express.Router();
 
-router.get("/formData", getFormData);
-router.post("/formDataPost", saveFormData);
+router.get("/formdata", getFormData);
+router.post("/formdatapost", saveFormData);
+
+router.get("/loungeandgrilldata", getLoungeAndGrillData);
+router.post("/loungeandgrilldatapost", saveLoungeAndGrillData);
+
+router.get("/naracafedata", getNaraCafeData);
+router.post("/naracafedataPost", saveNaraCafeData);
 
 module.exports = router;
