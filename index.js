a22a10c06a5123b45aa6d42e8e2d047eb5c1da82
Furquanflow <furqan.rahim@flowtechnologies.io>
2023-11-11 04:25:01 +0500

    Add S in http

--- index.js
+++ index.js
@@ -37,10 +37,11 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-let baseUrl = "http://lapashaform.vercel.app";
+let baseUrl = "https://lapashaform.vercel.app";
 
-app.post(`generate-and-send-pdf`, async (req, res) => {
+app.post(`/generate-and-send-pdf`, async (req, res) => {
   const formData = req.body.data;
+
   console.log("Working");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
