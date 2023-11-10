eaf8aee51f6cfde78d32170662921f53c765d1ae
Furquanflow <furqan.rahim@flowtechnologies.io>
2023-11-11 03:04:45 +0500

    Remove endpoint

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-let baseUrl = "https://lapashaform.vercel.app/eligibilityverificationview";
+let baseUrl = "https://lapashaform.vercel.app";
 
 app.post(`/generate-and-send-pdf`, async (req, res) => {
   const formData = req.body.data;
