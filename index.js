a47744c611c0521cbf84e5530a652213cdff0b26
Furquanflow <furqan.rahim@flowtechnologies.io>
2023-11-14 02:07:43 +0500

    Remove Body Parser

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 const PORT = process.env.port;
-// app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.json());
 app.use(routes);
 
