c7adcb86dee54685879d8466091122b8b2dc01c7
Furquanflow <furqan.rahim@flowtechnologies.io>
2023-12-12 04:38:07 +0500

    Add Command

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-  const express = require("express");
-  const mongoose = require("mongoose");
-  const cors = require('cors');
-  const routes = require("./router/Router");
-  const path = require("path")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./router/Router");
+const path = require("path");
 
-  require("dotenv").config();
-  const app = express();
-  app.use(cors());
-  const PORT = process.env.port;
-  app.use(express.json());
-  app.use(routes);
+require("dotenv").config();
+const app = express();
+app.use(express.json());
+const _dirName = path.dirname("");
+const buildPath = path.join(_dirName, "../lapashaform/build");
+app.use(express.static(buildPath));
+app.use(
+  cors({
+    origin: "*"
+  })
+);
+const PORT = process.env.port;
+app.use(routes);
 
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
 
-  mongoose
-    .connect(process.env.MONGODB_URL)
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.log(err));
-
-  app.listen(PORT, () => console.log(`Port Running at ${PORT}`));
+app.listen(PORT, () => console.log(`Port Running at ${PORT}`));
